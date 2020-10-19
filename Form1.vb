4a53b9a615bc301394c33acabe2f8ec68182aa34
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2020-10-19 18:21:41 +0900

    VB.NET練習(#1)

--- Form1.vb
+++ Form1.vb
@@ -1,13 +1,25 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ButtonExe.Click
-        Dim inputString As String
-        inputString = InputText.Text
-        If inputString = "1" Then
-            LabelNumber.Text = "5"
-        ElseIf inputString = "2" Then
-            LabelNumber.Text = "10"
-        Else
-            LabelNumber.Text = "なし"
-        End If
+        Dim sum As Integer
+        sum = 0
+        For i As Integer = 1 To 100
+            'If i = 50 Then
+            '    Continue For
+            'End If
+            If i >= 80 Then
+                Exit For
+            End If
+            sum += i
+        Next
+        LabelNumber.Text = sum
+        'Dim inputString As String
+        'inputString = InputText.Text
+        'If inputString = "1" Then
+        '    LabelNumber.Text = "5"
+        'ElseIf inputString = "2" Then
+        '    LabelNumber.Text = "10"
+        'Else
+        '    LabelNumber.Text = "なし"
+        'End If
     End Sub
 End Class
