fd745227cf2bb7af75d318d11f21457e687dcd94
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2020-10-21 15:06:15 +0900

    英単語アプリを作成(#1)

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,36 +22,37 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.ButtonExe = New System.Windows.Forms.Button()
-        Me.LabelAnswer = New System.Windows.Forms.Label()
+        Me.ButtonAnswer = New System.Windows.Forms.Button()
+        Me.LabelQuestion = New System.Windows.Forms.Label()
         Me.InputText = New System.Windows.Forms.TextBox()
-        Me.LabelNumber = New System.Windows.Forms.Label()
+        Me.LabelCorrectAnswer = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LabelQuestionNo = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ButtonBeforeQ = New System.Windows.Forms.Button()
+        Me.ButtonNextQ = New System.Windows.Forms.Button()
+        Me.LabelCorrect = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'ButtonExe
+        'ButtonAnswer
         '
-        Me.ButtonExe.Font = New System.Drawing.Font("MS UI Gothic", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
-        Me.ButtonExe.Location = New System.Drawing.Point(134, 225)
-        Me.ButtonExe.Name = "ButtonExe"
-        Me.ButtonExe.Size = New System.Drawing.Size(111, 57)
-        Me.ButtonExe.TabIndex = 0
-        Me.ButtonExe.Text = "答え"
-        Me.ButtonExe.UseVisualStyleBackColor = True
+        Me.ButtonAnswer.Font = New System.Drawing.Font("MS UI Gothic", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.ButtonAnswer.Location = New System.Drawing.Point(134, 225)
+        Me.ButtonAnswer.Name = "ButtonAnswer"
+        Me.ButtonAnswer.Size = New System.Drawing.Size(111, 57)
+        Me.ButtonAnswer.TabIndex = 0
+        Me.ButtonAnswer.Text = "答え"
+        Me.ButtonAnswer.UseVisualStyleBackColor = True
         '
-        'LabelAnswer
+        'LabelQuestion
         '
-        Me.LabelAnswer.AutoSize = True
-        Me.LabelAnswer.Font = New System.Drawing.Font("MS UI Gothic", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
-        Me.LabelAnswer.Location = New System.Drawing.Point(141, 140)
-        Me.LabelAnswer.Name = "LabelAnswer"
-        Me.LabelAnswer.Size = New System.Drawing.Size(101, 40)
-        Me.LabelAnswer.TabIndex = 1
-        Me.LabelAnswer.Text = "リンゴ"
+        Me.LabelQuestion.AutoSize = True
+        Me.LabelQuestion.Font = New System.Drawing.Font("MS UI Gothic", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.LabelQuestion.Location = New System.Drawing.Point(141, 140)
+        Me.LabelQuestion.Name = "LabelQuestion"
+        Me.LabelQuestion.Size = New System.Drawing.Size(101, 40)
+        Me.LabelQuestion.TabIndex = 1
+        Me.LabelQuestion.Text = "リンゴ"
         '
         'InputText
         '
@@ -61,15 +62,15 @@ Partial Class Form1
         Me.InputText.Size = New System.Drawing.Size(300, 35)
         Me.InputText.TabIndex = 2
         '
-        'LabelNumber
+        'LabelCorrectAnswer
         '
-        Me.LabelNumber.AutoSize = True
-        Me.LabelNumber.Font = New System.Drawing.Font("MS UI Gothic", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
-        Me.LabelNumber.Location = New System.Drawing.Point(480, 242)
-        Me.LabelNumber.Name = "LabelNumber"
-        Me.LabelNumber.Size = New System.Drawing.Size(37, 40)
-        Me.LabelNumber.TabIndex = 3
-        Me.LabelNumber.Text = "1"
+        Me.LabelCorrectAnswer.AutoSize = True
+        Me.LabelCorrectAnswer.Font = New System.Drawing.Font("MS UI Gothic", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.LabelCorrectAnswer.Location = New System.Drawing.Point(388, 242)
+        Me.LabelCorrectAnswer.Name = "LabelCorrectAnswer"
+        Me.LabelCorrectAnswer.Size = New System.Drawing.Size(37, 40)
+        Me.LabelCorrectAnswer.TabIndex = 3
+        Me.LabelCorrectAnswer.Text = "1"
         '
         'Label1
         '
@@ -81,15 +82,15 @@ Partial Class Form1
         Me.Label1.TabIndex = 4
         Me.Label1.Text = "第"
         '
-        'Label2
+        'LabelQuestionNo
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("MS UI Gothic", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
-        Me.Label2.Location = New System.Drawing.Point(173, 59)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(26, 28)
-        Me.Label2.TabIndex = 5
-        Me.Label2.Text = "1"
+        Me.LabelQuestionNo.AutoSize = True
+        Me.LabelQuestionNo.Font = New System.Drawing.Font("MS UI Gothic", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.LabelQuestionNo.Location = New System.Drawing.Point(173, 59)
+        Me.LabelQuestionNo.Name = "LabelQuestionNo"
+        Me.LabelQuestionNo.Size = New System.Drawing.Size(26, 28)
+        Me.LabelQuestionNo.TabIndex = 5
+        Me.LabelQuestionNo.Text = "1"
         '
         'Label3
         '
@@ -101,40 +102,51 @@ Partial Class Form1
         Me.Label3.TabIndex = 6
         Me.Label3.Text = "問"
         '
-        'Button1
+        'ButtonBeforeQ
         '
-        Me.Button1.Font = New System.Drawing.Font("MS UI Gothic", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
-        Me.Button1.Location = New System.Drawing.Point(115, 335)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(252, 57)
-        Me.Button1.TabIndex = 7
-        Me.Button1.Text = "前の問題"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.ButtonBeforeQ.Font = New System.Drawing.Font("MS UI Gothic", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.ButtonBeforeQ.Location = New System.Drawing.Point(115, 335)
+        Me.ButtonBeforeQ.Name = "ButtonBeforeQ"
+        Me.ButtonBeforeQ.Size = New System.Drawing.Size(252, 57)
+        Me.ButtonBeforeQ.TabIndex = 7
+        Me.ButtonBeforeQ.Text = "前の問題"
+        Me.ButtonBeforeQ.UseVisualStyleBackColor = True
         '
-        'Button2
+        'ButtonNextQ
         '
-        Me.Button2.Font = New System.Drawing.Font("MS UI Gothic", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
-        Me.Button2.Location = New System.Drawing.Point(467, 335)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(257, 57)
-        Me.Button2.TabIndex = 8
-        Me.Button2.Text = "次の問題"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.ButtonNextQ.Font = New System.Drawing.Font("MS UI Gothic", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.ButtonNextQ.Location = New System.Drawing.Point(467, 335)
+        Me.ButtonNextQ.Name = "ButtonNextQ"
+        Me.ButtonNextQ.Size = New System.Drawing.Size(257, 57)
+        Me.ButtonNextQ.TabIndex = 8
+        Me.ButtonNextQ.Text = "次の問題"
+        Me.ButtonNextQ.UseVisualStyleBackColor = True
+        '
+        'LabelCorrect
+        '
+        Me.LabelCorrect.AutoSize = True
+        Me.LabelCorrect.Font = New System.Drawing.Font("MS UI Gothic", 28.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.LabelCorrect.Location = New System.Drawing.Point(523, 234)
+        Me.LabelCorrect.Name = "LabelCorrect"
+        Me.LabelCorrect.Size = New System.Drawing.Size(68, 48)
+        Me.LabelCorrect.TabIndex = 9
+        Me.LabelCorrect.Text = "〇"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.LabelCorrect)
+        Me.Controls.Add(Me.ButtonNextQ)
+        Me.Controls.Add(Me.ButtonBeforeQ)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.LabelQuestionNo)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.LabelNumber)
+        Me.Controls.Add(Me.LabelCorrectAnswer)
         Me.Controls.Add(Me.InputText)
-        Me.Controls.Add(Me.LabelAnswer)
-        Me.Controls.Add(Me.ButtonExe)
+        Me.Controls.Add(Me.LabelQuestion)
+        Me.Controls.Add(Me.ButtonAnswer)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -142,13 +154,14 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents ButtonExe As Button
-    Friend WithEvents LabelAnswer As Label
+    Friend WithEvents ButtonAnswer As Button
+    Friend WithEvents LabelQuestion As Label
     Friend WithEvents InputText As TextBox
-    Friend WithEvents LabelNumber As Label
+    Friend WithEvents LabelCorrectAnswer As Label
     Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
+    Friend WithEvents LabelQuestionNo As Label
     Friend WithEvents Label3 As Label
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
+    Friend WithEvents ButtonBeforeQ As Button
+    Friend WithEvents ButtonNextQ As Button
+    Friend WithEvents LabelCorrect As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,17 +1,47 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ButtonExe.Click
-        Dim sum As Integer
-        sum = 0
-        For i As Integer = 1 To 100
-            'If i = 50 Then
-            '    Continue For
-            'End If
-            If i >= 80 Then
-                Exit For
-            End If
-            sum += i
-        Next
-        LabelNumber.Text = sum
+
+    Private japanese As String()
+    Private english As String()
+    Private questionNo As Integer
+
+    'ロード関数
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        japanese = {"リンゴ", "学校", "鉛筆", "机", "鞄", "本", "部屋", "コップ", "椅子", "頭"}
+        english = {"apple", "school", "pencil", "desk", "bag", "book", "room", "cup", "chair", "head"}
+        questionNo = 1
+        CreateQuestion(questionNo)
+        LabelQuestionNo.Text = questionNo
+        LabelQuestion.Text = japanese(0)
+        InputText.Text = ""
+        LabelCorrectAnswer.Text = ""
+        LabelCorrect.Text = ""
+        ButtonBeforeQ.Enabled = False
+    End Sub
+
+    '答えボタンクリック時の処理
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ButtonAnswer.Click
+        Dim answer As String = InputText.Text
+        LabelCorrectAnswer.Text = english(questionNo - 1)
+
+        If answer.Equals(english(questionNo - 1)) Then
+            LabelCorrect.Text = "〇"
+            LabelCorrect.ForeColor = Color.Red
+        Else
+            LabelCorrect.Text = "×"
+            LabelCorrect.ForeColor = Color.Blue
+        End If
+        'Dim sum As Integer
+        'sum = 0
+        'For i As Integer = 1 To 100
+        '    'If i = 50 Then
+        '    '    Continue For
+        '    'End If
+        '    If i >= 80 Then
+        '        Exit For
+        '    End If
+        '    sum += i
+        'Next
+        'LabelNumber.Text = sum
         'Dim inputString As String
         'inputString = InputText.Text
         'If inputString = "1" Then
@@ -23,4 +53,33 @@
         'End If
     End Sub
 
+    '前の問題のボタンクリック時の処理
+    Private Sub ButtonBeforeQ_Click(sender As Object, e As EventArgs) Handles ButtonBeforeQ.Click
+        questionNo -= 1
+        CreateQuestion(questionNo)
+    End Sub
+
+    '次の問題のボタンクリック時の処理
+    Private Sub ButtonNextQ_Click(sender As Object, e As EventArgs) Handles ButtonNextQ.Click
+        questionNo += 1
+        CreateQuestion(questionNo)
+    End Sub
+
+    '問題作成処理
+    Private Sub CreateQuestion(num As Integer)
+        LabelQuestionNo.Text = num
+        LabelQuestion.Text = japanese(num - 1)
+        InputText.Text = ""
+        LabelCorrectAnswer.Text = ""
+        LabelCorrect.Text = ""
+
+        If num <= 1 Then '最初の問題の時は
+            ButtonBeforeQ.Enabled = False '前の問題ボタンを無効
+        ElseIf num >= 10 Then '最後の問題の時は
+            ButtonNextQ.Enabled = False '次の問題ボタンを無効
+        Else
+            ButtonBeforeQ.Enabled = True '前の問題ボタンを有効
+            ButtonNextQ.Enabled = True '次の問題ボタンを有効
+        End If
+    End Sub
 End Class
